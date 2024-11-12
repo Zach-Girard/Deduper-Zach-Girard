52799451d03adef21e02c4a45bd45d0b938b1a4f
Zach Girard <zacharyleegirard@gmail.com>
2024-11-12 14:23:23 -0800

    Recent updates to Girard_deduper.py

--- Girard_deduper.py
+++ Girard_deduper.py
@@ -29,7 +29,7 @@ file.close()
 
 # Function to parse CIGAR string
 # This function returns a list of tuples
-# Example return: [(5,"S"),(84,"M")]
+# Example return: [(5,"S"),(84,"M")(4,"N"),(2,"S")]
 def CIGAR_smoke(CIGAR):
     parsed_list = []
     for match in re.finditer(r"(\d+)([MIDNSHP=X])", CIGAR):
@@ -37,6 +37,34 @@ def CIGAR_smoke(CIGAR):
         letter = match.group(2)
         parsed_list.append((number, letter))
     return parsed_list
+
+# Function to determine plus or minus strand via bitwise flag
+def determine_strand(flag):
+    if ((flag & 16) == 16):
+        strand = "minus"
+    else:
+        strand = "plus"
+    return strand
+
+# Function to adjust plus strand position if there is soft clipping
+def adjust_plus(position, parsed_list):
+    if parsed_list[0][1] == "S":
+        position -= parsed_list[0][0]
+    return position
+
+# Function to adjust minus strand position regardless of soft clipping
+def adjust_minus(position, parsed_list):
+   # If there is not soft clipping on the left side, normally adjust position
+    if parsed_list[0][1] != "S":
+        for i in range(len(parsed_list)):
+            if parsed_list[i][1] == "M" or "N" or "D" or "S":
+                position += parsed_list[i][0]
+    # If there is soft clipping on left side, skip it
+    else:
+        for i in range(1, len(parsed_list)):
+            if parsed_list[i][1] == "M" or "N" or "D" or "S":
+                position += parsed_list[i][0]
+    return position
 
 
 
@@ -55,8 +83,7 @@ headers = 0
 # Total non-duplicates written to output file
 lines_written = 0
 
-# Define Chromosome to clean Non-duplicate set and save memory
-Chrom = 1
+
 
 # Go through SAM file and place non-duplicates in output file
 with open(args.file, "r") as input, open(args.outfile, "w") as output:
@@ -73,67 +100,50 @@ with open(args.file, "r") as input, open(args.outfile, "w") as output:
         else:
             splitline = line.strip().split()
             # Chromosome of current read
-            Current_Chrom = splitline[2]
-            # Clear set if new Chromosome
-            if Current_Chrom != Chrom:
-                Non_duplicates.clear()
-                Chrom = Current_Chrom
-
+            Chrom = splitline[2]
+        
             # Q Name that contains UMI
             QNAME = splitline[0]
             QNAME = QNAME.split(":")
             # Pulling UMI from Q Name
-            UMI = QNAME[7]
+            UMI = QNAME[-1]
             # Pulling left most position of the read
             position = int(splitline[3])
             # pulling the CIGAR string
             CIGAR = splitline[5]
             
+            # Pulling bitwise flag
+            flag = int(splitline[1])
+            strand = determine_strand(flag)
+
+            # Call CIGAR parsing function
+            parsed_list = CIGAR_smoke(CIGAR)
+
             # Checking if there is an unknown UMI
             if UMI not in UMI_set:
                 unknown_UMIs += 1
 
             # If UMI is known, continue
             else:
-                # Pulling bitwise flag
-                flag = int(splitline[1])
-                # Checking for strandedness
-                if ((flag & 16) == 16):
-                    strand = "minus"
-                    # Call CIGAR parsing function
-                    parsed_list = CIGAR_smoke(CIGAR)
-                    # If there is not soft clipping on the left side, normally adjust position
-                    if parsed_list[0][1] != "S":
-                        for i in range(len(parsed_list)):
-                            if parsed_list[i][1] == "M" or "N" or "D" or "S":
-                                position = position + parsed_list[i][0]
-                    # If there is soft clipping on left side, skip it
-                    else:
-                        for i in range(1, len(parsed_list)):
-                            if parsed_list[i][1] == "M" or "N" or "D" or "S":
-                                position = position + parsed_list[i][0]
+                # adjust position based on strand and CIGAR string
+                if strand == "plus":
+                    position = adjust_plus(position, parsed_list)
 
-    
-                # If positive, check for soft clipping
                 else:
-                    strand = "plus"
-                    parsed_list = CIGAR_smoke(CIGAR)
-                    if parsed_list[0][1] == "S":
-                        position = position - parsed_list[0][0]
-                
+                    position = adjust_minus(position, parsed_list)
+
                 # If the unique read info has not been seen, write to file, add to count
-                if (position, strand, UMI) not in Non_duplicates:
-                    Non_duplicates.add((position, strand, UMI)) # type: ignore
+                if (position, strand, UMI, Chrom) not in Non_duplicates:
+                    Non_duplicates.add((position, strand, UMI, Chrom))
                     output.write(line)
                     lines_written += 1
+
 
                 # Counts duplicates 
                 else:
                     Duplicates += 1
-
-
-
 output.close()
+
 
 # Print stats
 print(f"Number of Duplicates: {Duplicates}")
